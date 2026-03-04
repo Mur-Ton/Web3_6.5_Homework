3de1f7fff9258e460aac3615492b09b60568ffab
Mur-Ton <1292952818@qq.com>
2026-03-04 22:35:17 +0800

    day3-homework0pollstation

--- day3-PollStation/day3_pollstation.sol
+++ day3-PollStation/day3_pollstation.sol
@@ -3,16 +3,16 @@ pragma solidity ^0.8.0;
 
 contract PollStation {
 
-    string[] public candidateNames;
-    mapping (string => bool) public isCandidate;
-    mapping (string => uint256) public voteCount;
-    mapping (address => bool) public isVoted;
+    string[] candidateNames;
+    mapping (string => bool) isCandidate;
+    mapping (string => uint256) voteCount;
+    mapping (address => bool) isVoted;
 
     // 添加候选人姓名
     function addCandidateNames (string memory _name) public {
-        candidateNames.push(_name);
-        voteCount[_name] = 0;
-        isCandidate[_name] = true;
+        candidateNames.push(_name); //添加候选人
+        voteCount[_name] = 0; //初始化候选人票数
+        isCandidate[_name] = true; //用于判断候选人是否存在
     }    
 
     // 检索候选人列表
@@ -21,10 +21,12 @@ contract PollStation {
     }
 
     // 为候选人投票篇
-    function voteForCandidate(string memory _name, address _voter) public {
-        require(validCandidate(_name));
-        voteCount[_name] += 1;
-        isVoted[_voter] = true;
+    function voteForCandidate(string memory _name) public {
+        if (!hasVoted(msg.sender)) { //若候选人已经投票，则isVoted[_voter] = true，取非不进判断
+            require(validCandidate(_name)); //判断候选人是否存在
+            voteCount[_name] += 1;
+            isVoted[msg.sender] = true; //判断候选人是否投票
+        }
     }
 
     // 检查候选人的票数
@@ -33,12 +35,12 @@ contract PollStation {
     }
 
     // 检查候选人是否存在
-    function validCandidate(string memory _name) public view returns (bool) {
+    function validCandidate(string memory _name) private view returns (bool) {
         return isCandidate[_name];
     }
 
-    // 检查投票人是否已经投过票
-    function hasVoted(address _voter) public view returns (bool) {
+    // 检查投票人是否重复投票
+    function hasVoted(address _voter) private view returns (bool) {
         return isVoted[_voter];
     }
 }
